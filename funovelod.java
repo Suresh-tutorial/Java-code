922cf52253ce4843fe47cf0d445a061c977f9874
Suresh-tutorial <sureshshanmugam1501@gmail.com>
2024-09-17 07:05:28 +0530

    created

---
+++ funovelod.java
@@ -0,0 +1,14 @@
+public class funovelod{
+    void sum(int a,int b){
+        System.out.print(a+b);
+
+    }
+
+    public static void main(String [] args){
+        funovelod obj = new funovelod();
+        obj.sum(2,5);
+        
+
+    }
+
+}
