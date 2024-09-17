b9035f5bba390f2c1956e8f7c05c8defc960256f
Suresh-tutorial <sureshshanmugam1501@gmail.com>
2024-09-17 07:15:29 +0530

    updated

--- funovelod.java
+++ funovelod.java
@@ -1,13 +1,21 @@
 public class funovelod{
+    // method overloading or funtion overloading
+    // we can't use same funtion name  twice 
+    // at same we can use same funtion name with different parameter is possible 
+    // for your ref given below
     void sum(int a,int b){
         System.out.print(a+b);
 
+    }
+    void sum(int a,int b ,int c){
+        System.out.print(a+b+c);
     }
 
     public static void main(String [] args){
         funovelod obj = new funovelod();
         obj.sum(2,5);
-        
+        obj.sum(7,9,1);
+
 
     }
 
